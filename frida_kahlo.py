2759c570099d972f84fd1237871dc4d171dbc8bd
Benjamin Patch <ben@benjaminpatch.com>
2025-02-25 16:44:14 -0800

    Add more paintings to the list.

--- frida_kahlo.py
+++ frida_kahlo.py
@@ -6,4 +6,11 @@ dates = [1939, 1933, 1946, 1940]
 
 # combine painting names and dates into a 2D list
 paintings = list(zip(paintings, dates))
+
+# add more paintings to the list
+paintings.append(('The Broken Column', 1944))
+paintings.append(('The Wounded Deer', 1946))
+paintings.append(('Me and My Doll', 1937))
+
+# output
 print(paintings)
