68f11d9f1f1e8f04495f3d31bd2a58b71af32e4e
Benjamin Patch <ben@benjaminpatch.com>
2025-02-25 17:28:43 -0800

    Generate audio tour numbers.

--- frida_kahlo.py
+++ frida_kahlo.py
@@ -12,5 +12,15 @@ paintings.append(('The Broken Column', 1944))
 paintings.append(('The Wounded Deer', 1946))
 paintings.append(('Me and My Doll', 1937))
 
+# find the number of paintings on our list
+number_of_paintings = len(paintings)
+
+# generate audio tour numbers
+audio_tour_numbers = []
+for num in range(7):
+  audio_tour_numbers.append(num + 1)
+
+print(audio_tour_numbers)
+
 # output
 print(paintings)
