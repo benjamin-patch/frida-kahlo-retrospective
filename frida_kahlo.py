f8066053fd6282e978ae7ea14f96af002657d809
Benjamin Patch <ben@benjaminpatch.com>
2025-02-25 16:32:54 -0800

    Combine painting names and dates into a 2D list.

--- frida_kahlo.py
+++ frida_kahlo.py
@@ -1,4 +1,9 @@
 # Frida Kahlo
 
+# raw data
 paintings = ['The Two Fridas', 'My Dress Hangs Here', 'Tree of Hope', 'Self Portrait With Monkeys']
 dates = [1939, 1933, 1946, 1940]
+
+# combine painting names and dates into a 2D list
+paintings = list(zip(paintings, dates))
+print(paintings)
