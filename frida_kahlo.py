14b2e8ff5bf36cc1f079b0031ef00a9d8c580ed8
Benjamin Patch <ben@benjaminpatch.com>
2025-02-26 11:23:31 -0800

    Create master list of tour numbers, painting titles and dates.

--- frida_kahlo.py
+++ frida_kahlo.py
@@ -4,13 +4,15 @@
 paintings = ['The Two Fridas', 'My Dress Hangs Here', 'Tree of Hope', 'Self Portrait With Monkeys']
 dates = [1939, 1933, 1946, 1940]
 
-# combine painting names and dates into a 2D list
-paintings = list(zip(paintings, dates))
-
 # add more paintings to the list
-paintings.append(('The Broken Column', 1944))
-paintings.append(('The Wounded Deer', 1946))
-paintings.append(('Me and My Doll', 1937))
+paintings.append('The Broken Column')
+paintings.append('The Wounded Deer')
+paintings.append('Me and My Doll')
+
+# include dates for the appended paintings above
+dates.append(1944)
+dates.append(1946)
+dates.append(1937)
 
 # find the number of paintings on our list
 number_of_paintings = len(paintings)
@@ -20,9 +22,15 @@ audio_tour_numbers = []
 for num in range(number_of_paintings):
   audio_tour_numbers.append(num + 1)
 
+# combine audio tour numbers, painting titles and dates into a single list
+master_list = list(zip(audio_tour_numbers, paintings, dates))
+
 # output
 print('Audio Tour Numbers:')
 print(audio_tour_numbers)
 
-print('Painting Titles and Dates:')
+print('Painting Titles:')
 print(paintings)
+
+print('Master List:')
+print(master_list)
